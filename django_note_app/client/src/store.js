c33517d177f3463170d0880dfa92f8d6c8545d37
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-04-12 11:18:59 +0100

    Add Redux store configuration to store.js

---
+++ django_note_app/client/src/store.js
@@ -0,0 +1,34 @@
+import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import rootReducer from "./Reducers";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["auth"], // Specify which reducers to persist
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+export default store;
