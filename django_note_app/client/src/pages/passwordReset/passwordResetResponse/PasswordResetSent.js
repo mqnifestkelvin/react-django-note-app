cdda09ff14891b5b39f2dfd1b9119831ad3939b2
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-04-12 11:38:54 +0100

    Add PasswordResetSent component to client/src/pages/passwordReset/passwordResetResponse/PasswordResetSent.js

---
+++ django_note_app/client/src/pages/passwordReset/passwordResetResponse/PasswordResetSent.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types"; // Import PropTypes
+
+export class PasswordResetSent extends Component {
+  render() {
+    return (
+      <div className="align-center">
+        <h1>Password Reset Sent</h1>
+      </div>
+    );
+  }
+}
+
+export default PasswordResetSent;
