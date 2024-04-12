825dc3c6d614e59b371700dff1ad7a4119a7bf86
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-04-12 11:39:35 +0100

    Add authentication reducer to handle user authentication in client/src/Reducers/Auth.js

---
+++ django_note_app/client/src/Reducers/Auth.js
@@ -0,0 +1,74 @@
+import {
+  USER_LOADED,
+  USER_LOADING,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT_SUCCESS,
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  PASSWORD_RESET_SUCCESS,
+  REFRESH_TOKEN_FAIL,
+  REFRESH_TOKEN_SUCCESS,
+} from "../Actions/Types";
+
+const initialState = {
+  token: localStorage.getItem("token"),
+  isAuthenticated: null,
+  isLoading: false,
+  user: null,
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case USER_LOADING:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        isLoading: false,
+        user: action.payload,
+      };
+    case LOGIN_SUCCESS:
+    case REGISTER_SUCCESS:
+      localStorage.setItem("token", action.payload.token);
+      return {
+        ...state,
+        ...action.payload,
+        isAuthenticated: true,
+        isLoading: false,
+      };
+    case AUTH_ERROR:
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        token: null,
+        user: null,
+        isAuthenticated: false,
+        isLoading: false,
+      };
+    case LOGIN_FAIL:
+    case LOGOUT_SUCCESS:
+    case REGISTER_FAIL:
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        token: null,
+        user: null,
+        isAuthenticated: false,
+        isLoading: false,
+      };
+    case PASSWORD_RESET_SUCCESS:
+      return {
+        ...state,
+        msg: action.payload.message,
+        status: action.payload.status,
+      };
+    default:
+      return state;
+  }
+}
